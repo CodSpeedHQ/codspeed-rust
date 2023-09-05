9caa8bd637ea44d4799edf08d5b8794a1f2b43c8
Arthur Pastel <arthur.pastel@gmail.com>
2023-09-05 17:39:22 +0200

    chore: bump all versions in the release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -12,5 +12,5 @@ git branch --show-current | grep -q '^main$'
 # Fail if there are any unstaged changes left
 git diff --exit-code
 
-cargo workspaces version --no-individual-tags --exact --no-git-push $1
+cargo workspaces version --no-individual-tags --exact --no-git-push --force "*" $1
 git push --follow-tags
