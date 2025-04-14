08752a0c270e6ab91f459bc2c12ff13aa356808c
not-matthias <matthias@codspeed.io>
2025-04-14 13:42:18 +0200

    chore: create shared module

--- crates/codspeed/src/fifo.rs
+++ crates/codspeed/src/fifo.rs
@@ -1,4 +1,5 @@
-use anyhow::{bail, Context};
+pub use super::shared::*;
+use anyhow::bail;
 use nix::libc::O_NONBLOCK;
 use nix::sys::stat;
 use nix::unistd::{self, unlink};
@@ -6,40 +7,6 @@ use std::fs::{File, OpenOptions};
 use std::io::Read;
 use std::os::unix::fs::OpenOptionsExt;
 use std::path::{Path, PathBuf};
-
-// !!!!!!!!!!!!!!!!!!!!!!!!
-// !! DO NOT TOUCH BELOW !!
-// !!!!!!!!!!!!!!!!!!!!!!!!
-// Has to be in sync with `runner`.
-//
-const RUNNER_CTL_FIFO_NAME: &str = "runner.ctl.fifo";
-const RUNNER_ACK_FIFO_NAME: &str = "runner.ack.fifo";
-
-pub fn runner_fifo_dir() -> anyhow::Result<PathBuf> {
-    let raw_path = std::env::var("CODSPEED_FIFO_DIR")
-        .context("CODSPEED_FIFO_DIR environment variable not set")?;
-    Ok(PathBuf::from(raw_path))
-}
-
-pub fn runner_ctl_fifo_path() -> anyhow::Result<PathBuf> {
-    runner_fifo_dir().map(|p| p.join(RUNNER_CTL_FIFO_NAME))
-}
-
-pub fn runner_ack_fifo_path() -> anyhow::Result<PathBuf> {
-    runner_fifo_dir().map(|p| p.join(RUNNER_ACK_FIFO_NAME))
-}
-
-#[derive(serde::Serialize, serde::Deserialize, Debug, PartialEq)]
-pub enum Command {
-    CurrentBenchmark { pid: u32, uri: String },
-    StartBenchmark,
-    StopBenchmark,
-    Ack,
-}
-//
-// !!!!!!!!!!!!!!!!!!!!!!!!
-// !! DO NOT TOUCH ABOVE !!
-// !!!!!!!!!!!!!!!!!!!!!!!!
 
 pub struct BenchGuard {
     ctl_fifo: FifoIpc,
--- crates/codspeed/src/lib.rs
+++ crates/codspeed/src/lib.rs
@@ -3,5 +3,6 @@ pub mod fifo;
 mod macros;
 mod measurement;
 mod request;
+mod shared;
 pub mod utils;
 pub mod walltime;
