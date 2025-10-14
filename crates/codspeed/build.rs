19f5244a309c05f529c3f80b669c1c3e3d1189bc
not-matthias <matthias@codspeed.io>
2025-10-14 14:07:47 +0200

    fix(codspeed): replace bindgen with manual bindings

--- crates/codspeed/build.rs
+++ crates/codspeed/build.rs
@@ -1,5 +1,3 @@
-use std::{env, path::PathBuf};
-
 fn main() {
     println!("cargo:rerun-if-changed=instrument-hooks/dist/core.c");
     println!("cargo:rerun-if-changed=instrument-hooks/includes/core.h");
@@ -29,18 +27,4 @@ fn main() {
 
         std::process::exit(1);
     }
-
-    let bindings = bindgen::Builder::default()
-        .header("instrument-hooks/includes/core.h")
-        // Tell cargo to invalidate the built crate whenever any of the
-        // included header files changed.
-        .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
-        .generate()
-        .expect("Unable to generate bindings");
-
-    // Write the bindings to the $OUT_DIR/bindings.rs file.
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-    bindings
-        .write_to_file(out_path.join("bindings.rs"))
-        .expect("Couldn't write bindings!");
 }
--- crates/codspeed/src/instrument_hooks/ffi.rs
+++ crates/codspeed/src/instrument_hooks/ffi.rs
@@ -3,4 +3,11 @@
 #![allow(non_snake_case)]
 #![allow(unused)]
 
-include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+// Use pre-generated bindings instead of generating at build time so that downstream
+// users don't need to install `libclang`.
+//
+// To regenerate bindings, run:
+// ```
+// ./update-bindings.sh
+// ```
+include!("bindings.rs");
