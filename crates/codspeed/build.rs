19ad9f2d315e591f5d7bdadbd83ca62b69f8c47d
not-matthias <matthias@codspeed.io>
2025-10-14 14:16:09 +0200

    fix(codspeed): fallback to noop when build fails

--- crates/codspeed/build.rs
+++ crates/codspeed/build.rs
@@ -1,12 +1,9 @@
 fn main() {
+    println!("cargo:rustc-check-cfg=cfg(use_instrument_hooks)");
+
     println!("cargo:rerun-if-changed=instrument-hooks/dist/core.c");
     println!("cargo:rerun-if-changed=instrument-hooks/includes/core.h");
     println!("cargo:rerun-if-changed=build.rs");
-
-    if cfg!(not(target_os = "linux")) {
-        // The instrument-hooks library is only supported on Linux.
-        return;
-    }
 
     let mut build = cc::Build::new();
     build
@@ -18,13 +15,17 @@ fn main() {
         .cargo_warnings(false);
 
     let result = build.try_compile("instrument_hooks");
-    if let Err(e) = result {
-        let compiler = build.try_get_compiler().expect("Failed to get C compiler");
+    match result {
+        Ok(_) => println!("cargo:rustc-cfg=use_instrument_hooks"),
+        Err(e) => {
+            let compiler = build.try_get_compiler().expect("Failed to get C compiler");
 
-        eprintln!("\n\nERROR: Failed to compile instrument-hooks native library with cc-rs. Ensure you have an up-to-date C compiler installed.");
-        eprintln!("Compiler information: {compiler:?}");
-        eprintln!("Compilation error: {e}");
+            eprintln!("\n\nWARNING: Failed to compile instrument-hooks native library with cc-rs.");
+            eprintln!("The library will still compile, but instrument-hooks functionality will be disabled.");
+            eprintln!("Compiler information: {compiler:?}");
+            eprintln!("Compilation error: {e}\n");
 
-        std::process::exit(1);
+            println!("cargo:warning=Failed to compile instrument-hooks native library with cc-rs. Continuing with noop implementation.");
+        }
     }
 }
