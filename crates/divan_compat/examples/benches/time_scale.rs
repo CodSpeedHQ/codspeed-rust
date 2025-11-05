ffec6ee41a47b76f52527065d5623100f023453f
Guillaume Lagrange <guillaume@glagrange.eu>
2025-11-05 11:32:47 +0100

    chore(divan_compat_examples): switch time_scale benches to busy sleeping
    Since the os thread scheduler is orders of magnitude ahead, it makes no
    sense to use thread::sleep for such small benchmarks

--- crates/divan_compat/examples/benches/time_scale.rs
+++ crates/divan_compat/examples/benches/time_scale.rs
@@ -2,27 +2,32 @@ fn main() {
     divan::main();
 }
 
+fn busy_sleeping(duration: std::time::Duration) {
+    let start = std::time::Instant::now();
+    while start.elapsed() < duration {}
+}
+
 #[divan::bench]
 fn sleep_1ns() {
-    std::thread::sleep(std::time::Duration::from_nanos(1));
+    busy_sleeping(std::time::Duration::from_nanos(1));
 }
 
 #[divan::bench]
 fn sleep_100ns() {
-    std::thread::sleep(std::time::Duration::from_nanos(100));
+    busy_sleeping(std::time::Duration::from_nanos(100));
 }
 
 #[divan::bench]
 fn sleep_1us() {
-    std::thread::sleep(std::time::Duration::from_micros(1));
+    busy_sleeping(std::time::Duration::from_micros(1));
 }
 
 #[divan::bench]
 fn sleep_100us() {
-    std::thread::sleep(std::time::Duration::from_micros(100));
+    busy_sleeping(std::time::Duration::from_micros(100));
 }
 
 #[divan::bench]
 fn sleep_1ms() {
-    std::thread::sleep(std::time::Duration::from_millis(1));
+    busy_sleeping(std::time::Duration::from_millis(1));
 }
